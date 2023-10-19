06398b4d60c0199cfb00a094d21edfef13ac5ef0
bekirbilgec <bekirbilgec42@gmail.com>
2023-10-19 16:59:03 +0300

    create files

---
+++ 02-variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// first variable
+	var name1 string
+	name1 = "Mehmet"
+
+	// second variable
+	var name2 string = "ahmet"
+
+	// third variable
+	name3 := "süleyman"
+
+	var age int
+	age = 25
+	var isMarried bool
+	isMarried = true
+
+	fmt.Println(name1)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
