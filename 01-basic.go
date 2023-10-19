5c9861d4ced5dc2df555ac85f283ea0bfa5f3104
bekirbilgec <bekirbilgec42@gmail.com>
2023-10-19 16:40:59 +0300

    create files

---
+++ 01-basic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const conferenceTickeds int = 50
+	var remainingTickets uint = 50
+	conferenceName := " Go Conference "
+
+	fmt.Printf(" Welcome to %v booking application ", remainingTickets)
+
+}
